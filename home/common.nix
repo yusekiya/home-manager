2153564a3162235996ac91d91966562abd22cf2e
yusekiya <y.seki.14870@gmail.com>
2026-09-17 22:42:07 +0900

    Add packages

--- home/common.nix
+++ home/common.nix
@@ -56,6 +56,7 @@
     librsvg
     llvm
     lua
+    lua-language-server
     mise
     neovim
     nkf
@@ -67,6 +68,7 @@
     ripgrep
     rsync
     rtk
+    ruff
     rustup
     sheldon
     ssh-copy-id
@@ -75,6 +77,7 @@
     tdf
     tealdeer
     terminaltexteffects
+    texlab
     tig
     tmux
     tree
